b3ed87a5362dcdcec0e7e10a9a72cac9f2a9f05e
myworldseb <59670478+myworldseb@users.noreply.github.com>
2020-01-09 09:28:33 -0300

    Completa pantalla registro + Completa pantalla principal coordinador + Se creo pantalla ficha general

--- app/src/main/java/com/example/apekantun/view/CreateAccountActivity.java
+++ app/src/main/java/com/example/apekantun/view/CreateAccountActivity.java
@@ -1,6 +1,7 @@
 package com.example.apekantun.view;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
@@ -12,5 +13,14 @@ public class CreateAccountActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+        showToolbar(getResources().getString(R.string.toolbar_tittle_registrar),false);
+
+    }
+    public void showToolbar(String tittle, boolean upButton){
+        Toolbar toolbar =findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(tittle);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(upButton);
+
     }
 }
---
+++ app/src/main/java/com/example/apekantun/view/PrincipalCoordinadorActivity.java
@@ -0,0 +1,16 @@
+package com.example.apekantun.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.apekantun.R;
+
+public class PrincipalCoordinadorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principal_coordinador);
+    }
+}
