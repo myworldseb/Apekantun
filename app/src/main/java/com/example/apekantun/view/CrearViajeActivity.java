733cd2d66ea9509b8c6b109b9e8317903b5ab59f
myworldseb <59670478+myworldseb@users.noreply.github.com>
2020-01-15 02:40:16 -0300

    Se modifico pantalla principal coordinador y se creo vista crear viaje

--- app/src/main/java/com/example/apekantun/view/CrearViajeActivity.java
+++ app/src/main/java/com/example/apekantun/view/CrearViajeActivity.java
@@ -4,13 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-import com.example.apekantun.R;
-
-public class FichaGeneralActivity extends AppCompatActivity {
+public class CrearViajeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_ficha_general);
+        setContentView(R.layout.activity_crear_viaje);
     }
 }
