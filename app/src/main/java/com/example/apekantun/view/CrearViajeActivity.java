0682cb17fb957b4e1f9faa0b3269163b9bb9ca8a
myworldseb <59670478+myworldseb@users.noreply.github.com>
2020-01-15 17:46:17 -0300

    Se modifico pantalla principal coordinador y se creo vista crear viaje

--- app/src/main/java/com/example/apekantun/view/CrearViajeActivity.java
+++ app/src/main/java/com/example/apekantun/view/CrearViajeActivity.java
@@ -2,13 +2,99 @@ package com.example.apekantun.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import com.example.apekantun.R;
+import androidx.appcompat.widget.Toolbar;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import java.sql.Date;
+import java.util.Calendar;
 
 public class CrearViajeActivity extends AppCompatActivity {
+EditText etfs;
+EditText etfr;
+private int code;
+private int mYearIni, mMonthIni, mDayIni,AñoSin, MesSin, DiaSin;
+    static final int DATE_ID = 0;
+Calendar C = Calendar.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_crear_viaje);
+        showToolbar(getResources().getString(R.string.toolbar_tittle_crear_viaje),false);
+        AñoSin = C.get(Calendar.YEAR);
+        MesSin = C.get(Calendar.MONTH);
+        DiaSin = C.get(Calendar.DAY_OF_MONTH);
+        etfr = (EditText) findViewById(R.id.fecha_realizar);
+        etfs = (EditText) findViewById(R.id.fecha_solicitud);
+        etfr.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDialog(DATE_ID);
+                code = 0;
+            }
+        });
+        etfs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDialog(DATE_ID);
+                code = 1;
+            }
+        });
     }
+
+
+    private void colocar_fecha(){
+        etfs.setText( mYearIni+ "-" +  (mMonthIni + 1)+ "-" + mDayIni +" ");
+    }
+    private void colocar_fechita(){
+        etfr.setText(mYearIni+ "-" +  (mMonthIni + 1)+ "-" + mDayIni +" ");
+    }
+
+    private DatePickerDialog.OnDateSetListener mDateSetListener =
+            new DatePickerDialog.OnDateSetListener() {
+                public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                    mYearIni = year;
+                    mMonthIni = monthOfYear;
+                    mDayIni = dayOfMonth;
+
+                    if(code == 0){
+                        colocar_fechita();
+                    }else{
+                        colocar_fecha();
+                    }
+
+
+                }
+
+            };
+
+
+@Override
+protected Dialog onCreateDialog(int id){
+    switch (id){
+        case DATE_ID:
+            return new DatePickerDialog(this, mDateSetListener,AñoSin, MesSin, DiaSin);
+    }
+    return null;
+        }
+
+
+    public void showToolbar(String tittle, boolean upButton){
+        Toolbar toolbar =findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(tittle);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(upButton);
+
+    }
+
 }
+
--- app/src/main/java/com/example/apekantun/view/PrincipalCoordinadorActivity.java
+++ app/src/main/java/com/example/apekantun/view/PrincipalCoordinadorActivity.java
@@ -1,8 +1,11 @@
 package com.example.apekantun.view;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.apekantun.R;
 
@@ -12,5 +15,18 @@ public class PrincipalCoordinadorActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal_coordinador);
+        showToolbar(getResources().getString(R.string.toolbar_tittle_menu_coordinador),false);
+    }
+    public void showToolbar(String tittle, boolean upButton){
+        Toolbar toolbar =findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(tittle);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(upButton);
+
+    }
+    public void goFichaGeneral(View view){
+        Intent intent = new Intent(this,FichaGeneralActivity.class );
+        startActivity(intent);
+
     }
 }
